3760aa2e525b1ff5dacc4ccdd3697c936a2ce6a2
ducp1605 <ducp1605@gmail.com>
2019-07-11 08:48:55 +0700

    Developed

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       Rectangle rectangle = new Rectangle(2.5, 3.8, "orange", true);
+        System.out.println(rectangle);
+
+        Circle circle = new Circle(3.5, "indigo", false);
+        System.out.println(circle);
+
+        Square square = new Square(5.8, "yellow", true);
+        System.out.println(square);
+
+
+    }
+}
